7bd66f59e668482792daa12cfb5ba67647c1c3d9
Joshua Bartholomew <joshua.c.bartholomew@gmail.com>
2023-05-05 15:27:01 -0500

    Added VPC definition.

--- cdk_example_scalable_web_app/cdk_example_scalable_web_app_stack.py
+++ cdk_example_scalable_web_app/cdk_example_scalable_web_app_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2,
 )
 from constructs import Construct
 
@@ -9,6 +9,8 @@ from constructs import Construct
 class CdkExampleScalableWebAppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        vpc = ec2.Vpc(self, "Vpc", ip_addresses=ec2.IpAddresses.cidr("10.16.0.0/16"))
 
         # The code that defines your stack goes here
 
